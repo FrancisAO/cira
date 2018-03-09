102114177d56df6508295f2beb9dea2f45936d3c
Francis Opoku <francis.opoku@bonprix.net>
2018-03-09 09:17:54 +0100

    Initial spring boot project

---
+++ src/main/java/com/cira/ontology_creation/OntologyCreationApplication.java
@@ -0,0 +1,12 @@
+package com.cira.ontology_creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologyCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntologyCreationApplication.class, args);
+	}
+}
